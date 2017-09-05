faf63b2b5fca51d99e9183a65dfbb1973b8643f0
mittalayushi <writetoayushi96@gmail.com>
2017-09-05 17:40:40 +0530

    helping functions made

---
+++ functions/functions.php
@@ -0,0 +1,68 @@
+<?php
+
+class URLShortener
+{
+	protected $db;
+
+	public function __construct()
+	{
+		$this->db = new mysqli("localhost","my_user","my_password","urlBank");
+		if($this->db->connect_errno)
+		{
+			header(("Error: 404"));
+			die();
+		}
+	}
+
+	public function generateCode($num)
+	{
+		$num=$num+10000000;
+		return base_convert($num,10,36);
+	}
+	public function returnShortCode($url)
+	{
+		$url=trim($url);
+		if(!filter_var($url,FILTER_VALIDATE_URL)){
+			header("Error: 404");
+			die();
+		}
+		else
+		{
+			$url=$this->db->real_escape_string($url);
+			$exist=$this->db->query("SELECT * FROM urlBank WHERE url='{$url}'");
+			if($exist->num_rows)
+			{
+				$code=$exist->fetch_object()->code;
+				return $code;
+			}
+			else
+			{
+				$insert=$this->db->query("INSERT INTO urlBank (url,time_stamp) VALUES ('{$url}',NOW())");
+				$fetch=$this->db->query("SELECT * FROM link WHERE url='{$url}'");
+				$get_id = $fetch->fetch_object()->id;
+				$secret = generateCode($get_id);
+
+				$update = $this->db->query("UPDATE urlBank SET shortcode = '{$secret}' WHERE url = '{$url}'");
+				return $secret;
+			}
+		}
+	}
+
+
+	public function returnShortCodeCustom($url,$custom){
+		$url = trim($url);
+		$custom = trim($custom);
+		if(filter_var($url,FILTER_VALIDATE_URL)){
+			$insert=$this->db->query("INSERT INTO urlBank(url,code,created) VALUES ('{$url}','{$custom}',NOW())");
+			return true;
+		}
+		return false;
+	}
+
+	 public function existsURL($short)
+    {
+        $short = $this->db->real_escape_string(strip_tags(addslashes($short)));
+        $rows = $this->db->query("SELECT url FROM link WHERE code = '{$short}' LIMIT 1");
+        return $rows->num_rows > 0;
+    }
+}
--- functions/shorten.php
+++ functions/shorten.php
@@ -4,3 +4,54 @@ require_once 'functions.php';
 $insertCustom = false;
 $errors = false;
 
+$shortener= new UrlShortener();
+
+if (($_POST['customCheck']=='on')&&(isset($_POST['custom'])))
+{
+	$custom=$_POST['custom'];
+
+	if(!$shortener->existsURL($custom))
+	{
+		$insertCustom=true;
+	}
+	else
+	{
+		$errors=true;
+		$_SESSION['error']="The custom URL is not available.";
+	}
+}
+
+if(isset($_POST['url'])&&!$errors)
+{
+	$url=$_POST['url'];
+	if(!$insertCustom)
+	{
+		if($code=$shortener->returnShortCode($url))
+		{
+			$_SESSION['success']=generateURL($code);
+		}
+		else
+		{
+			$_SESSION['error']="There was a problem.";
+		}
+	}
+	else
+	{
+		if($shortener->returnShortCodeCustom($url,$custom))
+		{
+			$_SESSION['success']=generateURL($custom);
+		}
+		else
+		{
+			header("Error 404");
+			die();
+		}
+	}
+}
+
+function generateURL($urlSuffix='')
+{
+	return "<a href='http://localhost/{$urlSuffix}'>http://localhost/{$urlSuffix}</a>";
+}
+
+
