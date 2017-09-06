c1bf7582bd4f9555e59236c788615c27ed86c96a
mittalayushi <writetoayushi96@gmail.com>
2017-09-06 18:39:35 +0530

    bug fixed

--- functions/functions.php
+++ functions/functions.php
@@ -22,27 +22,28 @@ class URLShortener
 	public function returnShortCode($url)
 	{
 		$url=trim($url);
+
 		if(!filter_var($url,FILTER_VALIDATE_URL)){
-			header("Error: 404");
+			header("Location: ../index.php?error=inurl");
 			die();
 		}
 		else
 		{
 			$url=$this->db->real_escape_string($url);
-			$exist=$this->db->query("SELECT * FROM urlBank WHERE url='{$url}'");
+			$exist=$this->db->query("SELECT * FROM urlbank WHERE url='{$url}'");
 			if($exist->num_rows)
 			{
-				$code=$exist->fetch_object()->code;
+				$code = $exist->fetch_object()->shortcode;
 				return $code;
 			}
 			else
 			{
-				$insert=$this->db->query("INSERT INTO urlBank (url,time_stamp) VALUES ('{$url}',NOW())");
+				$insert=$this->db->query("INSERT INTO urlbank (url,time_stamp) VALUES ('{$url}',NOW())");
 				$fetch=$this->db->query("SELECT * FROM link WHERE url='{$url}'");
 				$get_id = $fetch->fetch_object()->id;
 				$secret = generateCode($get_id);
 
-				$update = $this->db->query("UPDATE urlBank SET shortcode = '{$secret}' WHERE url = '{$url}'");
+				$update = $this->db->query("UPDATE urlbank SET shortcode = '{$secret}' WHERE url = '{$url}'");
 				return $secret;
 			}
 		}
@@ -52,8 +53,9 @@ class URLShortener
 	public function returnShortCodeCustom($url,$custom){
 		$url = trim($url);
 		$custom = trim($custom);
+		$url=$this->db->real_escape_string($url);
 		if(filter_var($url,FILTER_VALIDATE_URL)){
-			$insert=$this->db->query("INSERT INTO urlBank(url,code,created) VALUES ('{$url}','{$custom}',NOW())");
+			$insert=$this->db->query("INSERT INTO urlbank(url,code,created) VALUES ('{$url}','{$custom}',NOW())");
 			return true;
 		}
 		return false;
@@ -62,7 +64,7 @@ class URLShortener
 	 public function existsURL($short)
     {
         $short = $this->db->real_escape_string(strip_tags(addslashes($short)));
-        $rows = $this->db->query("SELECT url FROM urlBank WHERE shortcode = '{$short}' LIMIT 1");
+        $rows = $this->db->query("SELECT url FROM urlbank WHERE shortcode = '{$short}' LIMIT 1");
         return $rows->num_rows > 0;
     }
 }
